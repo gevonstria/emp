0a95273dc6f921b99be403243b7548a958afc0e3
gevonstria <gevtria@gmail.com>
2018-08-20 11:37:08 +0800

    Settin up master template

--- core/views.py
+++ core/views.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-	return render(request, "test.html")
+def master_template(request):
+	return render(request, "master/index.html")
